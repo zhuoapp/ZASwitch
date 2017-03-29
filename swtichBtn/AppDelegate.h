46bfb0c7cd7bb56893b12e007cabda0e0fb31c5c
zhaoyiyu <zhaoyiyu@zhuoapp.com>
2017-03-29 18:22:16 +0800

    第一次提交

---
+++ swtichBtn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  swtichBtn
+//
+//  Created by ZhuoApp on 2017/3/29.
+//  Copyright © 2017年 ZhuoApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
