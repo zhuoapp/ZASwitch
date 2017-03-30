af26190ec5fc6b055a4ab3310847417d9559ea6c
zhaoyiyu <zhaoyiyu@zhuoapp.com>
2017-03-30 10:15:36 +0800

    调整回调方法

---
+++ ZASwitchDemo/ViewController.m
@@ -0,0 +1,56 @@
+//
+//  ViewController.m
+//  ZASwitchDemo
+//
+//  Created by ZhuoApp on 2017/3/30.
+//  Copyright © 2017年 ZhuoApp. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ZASwitch.h"
+
+@interface ViewController ()<ZASwitchDelegate>
+{
+    ZASwitch * _swBtn;
+}
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    ZASwitch * switchView = [[ZASwitch alloc]initWithFrame:CGRectMake(100, 100, 40, 40)];
+    [self.view addSubview:switchView];
+    switchView.isOn = NO;
+    
+    switchView.delegate = self;
+    switchView.statusBlock = ^(ZASwitch * switchBtn){
+        NSLog(@"statusBlock——%d",switchBtn.isOn);
+    };
+    
+    [switchView setStatusChangedBlock:^(ZASwitch *switchBtn) {
+        
+    }];
+    
+    _swBtn = switchView;
+    
+    
+    UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.backgroundColor = [UIColor blackColor];
+    btn.frame = CGRectMake(100, 300, 200, 30);
+    [btn setTitle:@"改变颜色" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(changeSwitchColor) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)changeSwitchColor{
+    
+    _swBtn.onColor = [UIColor redColor];
+}
+#pragma mark -  ZASwitchDelegate
+-(void)switchBtnStatuChanged:(ZASwitch *)switchBtn{
+    NSLog(@"switchBtnStatuChanged——%d",switchBtn.isOn);
+}
+@end
---
+++ ZASwitchDemoTests/ZASwitchDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  ZASwitchDemoTests.m
+//  ZASwitchDemoTests
+//
+//  Created by ZhuoApp on 2017/3/30.
+//  Copyright © 2017年 ZhuoApp. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZASwitchDemoTests : XCTestCase
+
+@end
+
+@implementation ZASwitchDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
